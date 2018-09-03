96cc0fca39297a3542d5580d57ce987335bd0b90
yenille <devonbrazier@gmail.com>
2018-09-03 12:03:51 +0100

    Add optional telegram functionality

--- __main__.py
+++ __main__.py
@@ -1,6 +1,6 @@
 from rebalancer.testers import BackTester, LiveTester
 from telethon import TelegramClient, sync
-
+import os
 import yaml
 
 tester_types = {
@@ -8,11 +8,19 @@ tester_types = {
     "livetest": LiveTester
 }
 
-config_uri = "./rebalancer/config.yaml"
+api_id = os.environ.get('api_id')
+api_hash = os.environ.get('api_hash')
 
+if None in [api_id, api_hash]:
+    raise ValueError("Check environment variables")
+
+config_uri = "./rebalancer/config.yaml"
 conf = yaml.load(open(config_uri, "r"))
 
-client = TelegramClient('rebalancer', conf['api_id'], conf['api_hash']).start()
+if conf['telegram_on']:
+    client = TelegramClient('rebalancer', api_id, api_hash).start()
+else:
+    client = None
 
 if conf["tester_type"] in tester_types:
     if conf["tester_type"] == "backtest":
@@ -26,7 +34,8 @@ if conf["tester_type"] in tester_types:
         Tester = tester_types[conf["tester_type"]]  # insig time
         tester = Tester(conf)  # ~2.77489 seconds
 
-        tester.client = client
+        if conf['telegram_on']:
+            tester.client = client
 
         tester.start()
     else:
